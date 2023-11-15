b2544f228cc4fdaedd4fe3eaf4a57145aef22d97
David Levy <david@quix.ai>
2023-11-15 18:56:33 +0100

    Update

--- Printer Dashboard/src/app/components/chart/chart.component.ts
+++ Printer Dashboard/src/app/components/chart/chart.component.ts
@@ -15,7 +15,6 @@ import { ParameterData } from 'src/app/models/parameterData';
 export class ChartComponent implements OnInit {
   @ViewChild('canvas', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
   chart: Chart;
-  datasets: ChartDataset[] = [];
   options: ChartOptions = {
     interaction: {
       mode: 'nearest',
@@ -30,6 +29,10 @@ export class ChartComponent implements OnInit {
         realtime: {
           refresh: 500
         }
+      },
+      y: {
+        type: 'linear',
+        ticks: {}
       }
     },
     plugins: {
@@ -52,30 +55,63 @@ export class ChartComponent implements OnInit {
       }
     }
   };
+  dataset: ChartDataset<'line'> = {
+    data: [],
+    yAxisID: 'y',
+    pointHoverBorderWidth: 0,
+    pointRadius: 0,
+    borderColor: '#0088ff',
+    pointBackgroundColor: '#0088ff',
+  }
   configuration: ChartConfiguration = {
     type: 'line',
-    data: { datasets: this.datasets },
+    data: { datasets: [this.dataset] },
     options: this.options
   }
-  private _lastTimestamp: number;
   private _currentDelay: number;
-
-  @Input() key: string;
-  @Input() forecastKey: string;
-  @Input() range: { min: number, max: number };
+  private _key: string;
   @Input() reset$: Observable<void>;
-  // @Input() set range (range: { min: number, max: number }){
-  //   (this.options!.scales!['y'] as any).min = range.min - 10;
-  //   (this.options!.scales!['y'] as any).max = range.max + 10;
-  // }
+  @Input() set key(key: string) {
+    this.dataset.label = key;
+    this._key = key;
+  }
+  @Input() set color(color: string) {
+    this.dataset.borderColor = color;
+    this.dataset.pointBackgroundColor = color;
+  }
+  @Input() set label(label: string) {
+    this.dataset.label = label;
+  }
+  @Input() set range(range: { min: number, max: number }) {
+    (this.options as any).plugins.annotation.annotations['range'] = {
+      type: 'box',
+      yMin: range.min,
+      yMax: range.max,
+      backgroundColor: '#00FF000f',
+      borderWidth: 0
+    };
+    (this.options as any).scales['y'].min = range.min - 5;
+    (this.options as any).scales['y'].max = range.max + 5;
+  }
   @Input() set duration(duration: number) {
-    (this.options!.scales!['x'] as any).realtime['duration'] = duration;
+    (this.options as any).scales['x'].realtime['duration'] = duration;
   }
   @Input() set delay(delay: number) {
-    (this.options!.scales!['x'] as any).realtime['delay'] = delay;
+    (this.options as any).scales['x'].realtime['delay'] = delay;
+  }
+  @Input() set hiddenAxe(key: 'x' | 'y') {
+    (this.options as any).scales[key].ticks.display = false;
   }
   @Input() set data(data: ParameterData) {
-    this.onSetData(data);
+    const values = data?.numericValues![this._key];
+    if (values) {
+      data.timestamps?.forEach((timestamp, i) => {
+        this.dataset.data.push({ x: timestamp / 1000000, y: values[i] });
+      });
+      const lastTimestamp: number = data.timestamps[data.timestamps.length - 1];
+      this.updateDelay(lastTimestamp);
+    }
+    this.chart?.update();
   }
 
   constructor() {
@@ -95,88 +131,15 @@ export class ChartComponent implements OnInit {
   ngOnInit(): void {
     const ctx = this.canvas.nativeElement.getContext('2d');
     this.chart = new Chart(ctx!, this.configuration);
-
     this.reset$.subscribe(() => this.reset());
   }
 
-  onSetData(data: ParameterData) {
-    const values = data?.numericValues![this.key];
-    if (values) {
-      data.timestamps?.forEach((timestamp, i) => {
-        this.updateChart(this.key, { x: timestamp / 1000000, y: values[i] }, '#008eff')
-        this._lastTimestamp = timestamp;
-      });
-
-      if (this.forecastKey) {
-        (this.options.plugins!.annotation!.annotations as any)['forecast'] = {
-          type: 'line',
-          xMin: this._lastTimestamp / 1000000,
-          xMax: this._lastTimestamp / 1000000,
-          borderColor: 'rgb(255, 99, 132)',
-          borderWidth: 2,
-        }
-
-        // Remove initial values of forecast
-        const dataset = this.datasets.find((f) => f.label === this.forecastKey);
-        if (dataset) dataset.data = dataset.data.filter((f: any) => f.x > this._lastTimestamp / 1000000);
-      }
-    }
-
-    const forecastValues = data?.numericValues![this.forecastKey];
-    if (forecastValues) {
-      // remove previous forecast
-      const dataset = this.datasets.find((f) => f.label === this.forecastKey);
-      if (dataset) dataset.data = []
-
-      data.timestamps?.forEach((timestamp, i) => {
-        if (timestamp < this._lastTimestamp) return;
-        this.updateChart(this.forecastKey, { x: timestamp / 1000000, y: forecastValues[i] }, '#ca5fff', false)
-      });
-    }
-
-
-    if (this.range) {
-      (this.options.plugins!.annotation!.annotations as any)['range'] = {
-        type: 'box',
-        yMin: this.range.min,
-        yMax: this.range.max,
-        backgroundColor: 'rgba(255, 99, 132, 0.25)'
-      }
-    }
-
-    this.chart?.update();
-  }
-
-  updateChart(key: string, point: { x: number, y: number }, color?: string, hideAxis?: boolean): void {
-    const delay = Date.now() - point.x;
+  updateDelay(timestamp: number): void {
+    const delay = Date.now() - timestamp / 1000000;
     const offset = 1000;
     if (!this._currentDelay || this._currentDelay - delay > offset || this._currentDelay - delay < -offset) {
       (this.options!.scales!['x'] as any).realtime.delay = delay + offset;
       this._currentDelay = delay;
-    }
-
-    let dataset = this.datasets.find((f) => f.label === key);
-    if (dataset) {
-      dataset.data.push(point);
-    } else {
-      let options: any = {
-        type: 'linear',
-        axis: 'y',
-        display: !hideAxis,
-        min: this.range.min - 10,
-        max: this.range.max + 10
-      };
-      let dataset: ChartDataset<'line'> = {
-        data: [point],
-        label: key,
-        yAxisID: key,
-        borderColor: color,
-        pointBackgroundColor: color,
-        pointHoverBorderWidth: 0,
-        pointRadius: 0
-      };
-      this.options.scales![key] = options;
-      this.datasets.push(dataset);
     }
   }
 
