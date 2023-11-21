a1bad7719c3e73f759ca171fbc8231d728aaa7d7
Jordi Vicedo <jordi@quix.ai>
2023-11-21 09:26:46 +0100

    Set name of downsampled stream

--- Down-sampling/main.py
+++ Down-sampling/main.py
@@ -58,7 +58,7 @@ def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
     # When input stream closes, we close output stream as well.
     def on_stream_close(stream_consumer: qx.StreamConsumer, end_type: qx.StreamEndType):
         stream_producer.close()
-        print("Stream closed:" + stream_producer.stream_id)
+        print(f"Stream closed: {stream_producer.stream_id} ({stream_producer.properties.name})")
 
     stream_consumer.on_stream_closed = on_stream_close
 
