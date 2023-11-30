7c1d9d72fd58544e7556de890ca0f17aab9a0586
Jordi Vicedo <jordi@quix.ai>
2023-11-30 13:47:25 +0100

    Adjust delays

--- Data Generator/main.py
+++ Data Generator/main.py
@@ -159,8 +159,8 @@ async def generate_data_and_close_stream_async(topic_producer: qx.TopicProducer,
         print(f"{printer}: Closing stream")
         stream.close()
 
-        # Wait 5 minutes before starting again
-        await asyncio.sleep(5 * 60)
+        # Wait 10 seconds before starting again
+        await asyncio.sleep(10)
 
 
 async def main():
@@ -183,8 +183,8 @@ async def main():
         # Set stream ID or leave parameters empty to get stream ID generated.
         name = f"Printer {i + 1}"  # We don't want a Printer 0, so start at 1
 
-        # Start sending data, each printer will start 5 minutes after the previous one
-        tasks.append(asyncio.create_task(generate_data_and_close_stream_async(topic_producer, name, i * 20)))
+        # Start sending data, each printer will start 1 minute after the previous one
+        tasks.append(asyncio.create_task(generate_data_and_close_stream_async(topic_producer, name, i * 60)))
 
     await asyncio.gather(*tasks)
 
