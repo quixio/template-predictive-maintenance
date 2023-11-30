54f0d673c0b7255694e00022692d9c208255c836
Tony Bedford <tony+demo@quix.io>
2023-11-30 13:37:14 +0000

    Edited 'main.py'

--- Alerts Filter/main.py
+++ Alerts Filter/main.py
@@ -16,8 +16,8 @@ def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.Dat
 
 # Handle event data from samples that emit event data
 def on_event_data_received_handler(stream_consumer: qx.StreamConsumer, data: qx.EventData):
-    if data.id == 'over-alert':
-        print(data)
+    #if data.id == 'over-alert':
+    print(data)
 
 
 
