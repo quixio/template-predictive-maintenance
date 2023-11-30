c524eec700c8a1fe6170f7a459bb8bedb542309f
Tony Bedford <tony+demo@quix.io>
2023-11-30 13:30:47 +0000

    Edited 'main.py'

--- Alerts Filter/main.py
+++ Alerts Filter/main.py
@@ -20,7 +20,9 @@ def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.Dat
 def on_event_data_received_handler(stream_consumer: qx.StreamConsumer, data: qx.EventData):
     # print('===============================>>>>>>>>>>>>>', data)
     # handle your event data here
-    print('============')
+    if data['status'] == 'over-forecast':
+        print('============')
+
 
 
 def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
