71949d80a20879d257fbbed2b1a26c9767ad646c
Merlin Carter <merlin+demo@quix.io>
2023-12-05 10:17:32 +0000

    Edited 'main.py'

--- Starter transformation SDF/main.py
+++ Starter transformation SDF/main.py
@@ -6,7 +6,7 @@ from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseries
 app = Application.Quix("transformation-v1", auto_offset_reset="latest")
 
 input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
-output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
+# output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
 sdf = app.dataframe(input_topic)
 
@@ -14,7 +14,7 @@ sdf = app.dataframe(input_topic)
 
 sdf = sdf.update(lambda row: print(row))
 
-sdf = sdf.to_topic(output_topic)
+# sdf = sdf.to_topic(output_topic)
 
 if __name__ == "__main__":
     app.run(sdf)
