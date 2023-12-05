74ef797881c9669ae6a290953334cfef4988a627
Merlin Carter <merlin+demo@quix.io>
2023-12-05 10:30:09 +0000

    Edited 'main.py'

--- Starter transformation SDF/main.py
+++ Starter transformation SDF/main.py
@@ -3,7 +3,7 @@ from quixstreams import Application, State
 from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer
 
 
-app = Application.Quix("transformation-v2", auto_offset_reset="latest")
+app = Application.Quix("transformation-v3", auto_offset_reset="latest")
 
 input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
 # output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
@@ -16,7 +16,7 @@ try:
     sdf = sdf[["Timestamp","Speed","Gear"]]
     # def rolling_speed(row: dict, state: State)
     sdf = sdf.update(lambda row: print(row))
-    
+
 except Exception as e:
     print(f"Exception {e}")
 
