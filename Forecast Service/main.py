3f785218f42457296fda5f793f27421dc17c7c24
SteveRosam <steve@quix.io>
2024-02-29 18:28:43 +0000

    pythonification

--- Forecast Service/main.py
+++ Forecast Service/main.py
@@ -1,5 +1,4 @@
-from uuid import uuid4
-from quixstreams import Application, message_context
+from quixstreams import Application
 from dotenv import load_dotenv
 
 import logging
@@ -18,20 +17,21 @@ with open("./.env", 'a+') as file: pass  # make sure the .env file exists
 load_dotenv("./.env") # load environment variables from .env file for local dev
 
 # Assigning environ vars to local variables
-topic_input = os.environ["input"]
-topic_output = os.environ["output"]
-parameter_name = os.environ["parameter_name"] if "parameter_name" in os.environ else "fluctuated_ambient_temperature"
+topic_input = os.getenv("input", "downsampled-3d-printer-data-json")
+topic_output = os.getenv("output", "forecast")
 
-forecast_length = int(os.environ['forecast_length'])
+forecast_length = int(os.getenv('forecast_length', "5"))
 
-debug = os.environ["debug"] == "1" if "debug" in os.environ else False
+debug = os.getenv("debug", False)
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG if debug else logging.INFO)
 logger = logging.getLogger(__name__)
 
 # 
 def on_message_handler(rows: List[dict]):
-    
-    print(rows)
+    """
+    Run the prediciton using the sklearn
+    Output a list of objects with timestamp and forecast
+    """
 
     forecast_input = list(map(lambda row: row["mean_fluctuated_ambient_temperature"], rows))
 
@@ -58,49 +58,47 @@ def on_message_handler(rows: List[dict]):
 
 
 if __name__ == "__main__":
-    
+   
+    # Quix platform injects credentials automatically to the client.
+    # Alternatively, you can always pass an SDK token manually as an argument when working locally.
+    # Or set the relevant values in a .env file
     app = Application.Quix("transformation", auto_offset_reset="earliest")
 
     # Change consumer group to a different constant if you want to run model locally.
     logger.info("Opening input and output topics")
 
+    # Open the topics for input and output of data
     input_topic = app.topic(topic_input, value_deserializer="json")
     producer_topic = app.topic(topic_output, value_serializer="json")
 
     # Hook up to termination signal (for docker image) and CTRL-C
     logger.info("Listening to streams. Press CTRL-C to exit.")
 
-    sdf = app.dataframe(input_topic)
+    sdf = app.dataframe(input_topic)  # initialize the streaming dataframe
 
-    sdf = sdf[sdf.contains("mean_fluctuated_ambient_temperature")]  # ensure the parameter_name column exists in the incomming data
+    sdf = sdf[sdf.contains("mean_fluctuated_ambient_temperature")]  # ensure the column exists in the incomming data
 
-    sdf = sdf[["timestamp", "mean_fluctuated_ambient_temperature"]]
+    sdf = sdf[["timestamp", "mean_fluctuated_ambient_temperature"]]  # select only these coluns for processing
 
-    # reducer appends all row values (in this window) to state
+    # reducer appends all but first rows values (in this window) to state
     def reducer(state: list, row: dict):
         state.append(row)
         return state
 
-    # init initializes the state. Arrays with the first value
+    # init initializes the state arrays with the first value
     def reducer_init(row: dict):
         return [row]
 
     # apply the reducer funciton
     sdf = sdf.tumbling_window(5000).reduce(reducer, reducer_init).final()
-    #sdf = sdf.update(lambda message: print(message))
-
-    # sdf = sdf.update(lambda message: print(message))
-
-
-    # sdf = sdf.update(lambda message: logger.info(message))
 
     # now handle the output from the above stage which will be like {value: {key:value}}
     sdf = sdf.apply(lambda row: on_message_handler(row["value"]), expand=True)
-    # uncomment to log the raw data to console.
 
+    # convert the timestamps to human readable
     sdf["timestamp"] = sdf["timestamp"].apply(lambda epoch: str(datetime.fromtimestamp(epoch/1000)))
-    sdf = sdf.update(lambda message: print(message))
 
+    # publish the data resulting from this pipline to the topic
     sdf = sdf.to_topic(producer_topic)
 
     try:
